36b2a11b3d4b92846aa4a335608332deae75ea6d
Andrey Popov <mr.a.popov@gmail.com>
2025-06-01 16:38:27 -1000

    Refactor.

--- common-merge-structure/src/main/java/community/redrover/merge/util/FileUtils.java
+++ common-merge-structure/src/main/java/community/redrover/merge/util/FileUtils.java
@@ -13,7 +13,6 @@ import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Objects;
 
-
 public class FileUtils {
 
     public static Map<String, Object> loadFileToMap(Path path) throws IOException {
--- common-merge-structure/src/test/java/community/redrover/merge/util/FileUtilsTest.java
+++ common-merge-structure/src/test/java/community/redrover/merge/util/FileUtilsTest.java
@@ -15,7 +15,6 @@ import java.util.Map;
 import java.util.Objects;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class FileUtilsTest {
 
     @Getter
