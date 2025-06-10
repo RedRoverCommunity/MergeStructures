db695266edea8f1e3b031f24f95bb269d75eb46c
Andrey Popov <mr.a.popov@gmail.com>
2025-06-10 16:09:05 -0500

    Add @SuppressWarnings("unused") to both ExtendStrategyConfig constructors.

--- common-merge-structure/src/main/java/community/redrover/merge/model/config/ExtendStrategyConfig.java
+++ common-merge-structure/src/main/java/community/redrover/merge/model/config/ExtendStrategyConfig.java
@@ -4,10 +4,12 @@ import community.redrover.merge.model.Strategy;
 
 public class ExtendStrategyConfig extends AbstractStrategyConfig {
 
+    @SuppressWarnings("unused")
     public ExtendStrategyConfig() {
         super();
     }
 
+    @SuppressWarnings("unused")
     public ExtendStrategyConfig(Strategy strategy, String sourceFile, String targetFile, String resultFile) {
         super(strategy, sourceFile, targetFile, resultFile);
     }
