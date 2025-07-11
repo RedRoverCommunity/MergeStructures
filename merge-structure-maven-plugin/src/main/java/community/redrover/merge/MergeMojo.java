a9a7febd0ab47aa302d1fd1b4359bbee924f524d
Andrey Popov <mr.a.popov@gmail.com>
2025-07-11 00:58:35 -1000

    Refactor plugin Mojo classes.

--- merge-structure-maven-plugin/src/main/java/community/redrover/merge/MergeMojo.java
+++ merge-structure-maven-plugin/src/main/java/community/redrover/merge/MergeMojo.java
@@ -2,21 +2,11 @@ package community.redrover.merge;
 
 import community.redrover.merge.model.Strategy;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 
 @SuppressWarnings("unused")
 @Mojo(name = "merge", defaultPhase = LifecyclePhase.GENERATE_RESOURCES, threadSafe = true)
 public class MergeMojo extends BaseMojo {
-
-    @Parameter(property = "merge.sourceFile", required = true)
-    private String sourceFile;
-
-    @Parameter(property = "merge.destinationFile", required = true)
-    private String destinationFile;
-
-    @Parameter(property = "merge.resultFile", required = true)
-    private String resultFile;
 
     @Override
     protected Strategy getStrategy() {
